8ba2183749f120d9efc2e6eb64a7fc993bc467f0
mpiccolomo <m.piccolomo@nesea.it>
2021-06-18 17:07:13 +0200

    mod tabella Articoli

--- src/app/articoli/articoli.component.ts
+++ src/app/articoli/articoli.component.ts
@@ -1,4 +1,6 @@
+import { ArticoliDataService } from './../services/data/articoli-data.service';
 import { Component, OnInit } from '@angular/core';
+
 
 export class Articoli {
 
@@ -23,16 +25,32 @@ export class Articoli {
 })
 export class ArticoliComponent implements OnInit {
 
+  NumArt = 0;
+
+  articoli: Articoli[] | undefined ;
+
+  /*
   articoli = [
     new Articoli('014600301','BARILLA FARINA 1 KG','PZ',24,1,1.09,true,new Date()),
     new Articoli('013500121','BARILLA PASTA GR.500 N.70 1/2 PENNE','PZ',30,0.5,1.3,true,new Date()),
     new Articoli('007686402','FINDUS FIOR DI NASELLO 300 GR','PZ',8,0.3,6.46,true,new Date()),
     new Articoli('057549001','FINDUS CROCCOLE 400 GR','PZ',12,0.4,5.97,true,new Date())
   ]
+  */
 
-  constructor() { }
+  constructor(private articoliService: ArticoliDataService) { }
 
   ngOnInit(): void {
+
+    this.articoliService.getArticoli('Barilla').subscribe(
+      response => {
+
+        console.log(response);
+        this.articoli = response;
+        this.NumArt = this.articoli.length
+      }
+
+    )
   }
 
 }
---
+++ src/app/services/data/articoli-data.service.ts
@@ -0,0 +1,19 @@
+i
+
+mport { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Articoli } from 'src/app/articoli/articoli.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticoliDataService {
+
+  constructor(private httpClient : HttpClient) { }
+
+  getArticoli(descrizione : string) {
+
+    return this.httpClient.get<Articoli[]>(`http://localhost:5051/api/articoli/cerca/descrizione/${descrizione}`); //ALT + 0096 | ALT GR + '
+
+  }
+}
